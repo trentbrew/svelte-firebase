8af341150e691634f99a1ddc841d1d74cdf76e10
Brew <TBrew@hbrconsulting.com>
2020-03-19 01:10:37 -0500

    progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,34 @@
 <script>
-	export let name;
+	import { fade, fly } from 'svelte/transition';
+
+	export let name; //can be used in child components
+
+	let rando; //scoped ONLY to this component
+
+	$: result = (Math.round(rando * 100)); 
+
+	function setRando() {
+		rando = Math.random();
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<!--p class="num">Number: {rando}</p-->
+	<hr>
+	<p>{ result }</p> <!-- a boolen case -->
+	<button class="button" on:click={setRando}>Randomize</button>
+	
+	<p>Your score is { result }</p>
+
+	<!--conditionals!-->
+	{#if result > 75}
+		<p transition:fade>top 25%</p>
+	{:else if result > 50}
+		<p in:fly={{x: 60, duration: 500}} out:fly={{x: -60, duration: 500}}>top 50%</p>
+	{:else}
+		<p>do better:p</p>
+	{/if}
 </main>
 
 <style>
@@ -16,10 +40,22 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #335499;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 2em;
 		font-weight: 100;
+	}
+
+	.button {
+		background: green;
+		border:none;
+		border-radius: 12px;
+		color: white;
+		cursor: pointer;
+	}
+
+	.button:hover {
+		filter: brightness(0.6);
 	}
 
 	@media (min-width: 640px) {
