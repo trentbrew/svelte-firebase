a6dcd19ef71acfa7dfeedf3d29002204afe32335
Trenton Brew <32501733+tbrew1023@users.noreply.github.com>
2022-02-05 04:29:03 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 var firebaseConfig = {
-    apiKey: "AIzaSyAStBChEKaGzwaBhJSY6dsMYBXFgRPWl2o",
-    authDomain: "svelte-starter-ccff2.firebaseapp.com",
-    databaseURL: "https://svelte-starter-ccff2.firebaseio.com",
-    projectId: "svelte-starter-ccff2",
-    storageBucket: "svelte-starter-ccff2.appspot.com",
-    messagingSenderId: "365885259161",
-    appId: "1:365885259161:web:718657e35b6ed11b7fe708",
-    measurementId: "G-D14ZWVEE5E"
+
 }
 
 console.log(firebase);
